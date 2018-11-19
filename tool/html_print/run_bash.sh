4df3b3cf7a37936511a4c5f5942906ac702ec492
Yuya Kato <yuyakato@gmail.com>
2018-11-20 01:47:33 +0900

    HTMLを印刷するためのツールを追加

---
+++ tool/html_print/run_bash.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -eu
+IMAGE_NAME=thermal-printer/tool/html_print
+cd -- `dirname -- ${0}`
+docker run --interactive --tty --rm \
+  --publish 3032:8080 \
+  --volume $(pwd):/workspace \
+  ${IMAGE_NAME} \
+  /bin/bash
---
+++ tool/html_print/run_service.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -eu
+IMAGE_NAME=thermal-printer/tool/html_print
+CONTAINER_NAME=thermal-printer_tool_html_print
+docker stop ${CONTAINER_NAME} || true
+docker rm   ${CONTAINER_NAME} || true
+docker run --detach \
+  --restart unless-stopped \
+  --publish 3032:8080 \
+  --name ${CONTAINER_NAME} \
+  ${IMAGE_NAME}
