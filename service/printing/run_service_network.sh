5f5e15b88d46cb88a388cd4503285591c2b1d766
Yuya Kato <yuyakato@gmail.com>
2019-02-18 01:59:50 +0900

    ネットワーク接続のサーマルプリンタ用のサービス起動シェルスクリプトを追加

---
+++ service/printing/run_service_network.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -eu
+cd -- `dirname -- $0`
+source ./env.sh
+docker stop ${CONTAINER_NAME} || true
+docker rm   ${CONTAINER_NAME} || true
+docker run --detach \
+  --publish 3030:8080 \
+  --env PRINTER_TYPE=NETWORK \
+  --env PRINTER_HOST \
+  --env PRINTER_PORT \
+  --name ${CONTAINER_NAME} \
+  ${IMAGE_NAME}
