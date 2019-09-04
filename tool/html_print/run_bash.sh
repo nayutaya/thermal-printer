56d7d882f265afc99dae974cbac203a1da48b345
Yuya Kato <yuyakato@gmail.com>
2019-09-04 14:17:46 +0900

    nodeをバージョンアップ

--- tool/html_print/run_bash.sh
+++ tool/html_print/run_bash.sh
@@ -2,7 +2,7 @@
 set -eu
 IMAGE_NAME=thermal-printer/tool/html_print
 cd -- `dirname -- ${0}`
-docker run --interactive --tty --rm \
+docker container run --interactive --tty --rm \
   --publish 3032:8080 \
   --volume $(pwd):/workspace \
   ${IMAGE_NAME} \
